3f5123da2e2c50c3e653191b532f5c08d3face79
somesing-io <45449282+somesing-io@users.noreply.github.com>
2024-03-22 14:38:04 +0900

    Update KIP7Token.sol
    decimal, totalsupply 고정

--- KIP7Token.sol
+++ KIP7Token.sol
@@ -7,7 +7,7 @@ import "./KIP7Metadata.sol";
 import "./KIP7Lockable.sol";
 
 contract KIP7Token is KIP7Mintable, KIP7Burnable, KIP7Pausable, KIP7Metadata, KIP7Lockable {
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) KIP7Metadata(name, symbol, decimals) public {
-        _mint(msg.sender, initialSupply);
+    constructor(string memory name, string memory symbol) KIP7Metadata(name, symbol, 18) public {
+        _mint(msg.sender, 5000000000000000000000000000);
     }
 }
